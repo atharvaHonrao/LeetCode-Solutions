24f5d6c2a0dd90557da69722ee018b8da3809a3d
atharvaHonrao <111292844+atharvaHonrao@users.noreply.github.com>
2024-02-10 00:10:29 +0530

    Update Sort_Colors.py
    Runtime Beats 84.78% of users with Python
    Memory Beats 91.40% of users with Python

--- Sort_Colors.py
+++ Sort_Colors.py
@@ -1,23 +1,19 @@
 class Solution(object):
     def sortColors(self, nums):
-        index = 0
-        b = False
-        for i in range (0,len(nums)):
-            if nums[i]==2:
-                nums.append(2)
-                nums[i]=-1
-            if nums[i] == 1 and b == False:
-                index = i
-                b = True
-            if nums[i] == 0:
-                if b == True:
-                    temp = nums[i]
-                    nums[i] = nums[index]
-                    nums[index] = temp
-                    index = index + 1
+        x = 0
+        y = 0
+        for i in range(0,len(nums)):
+            if nums[i]==0 and y<len(nums) and x<len(nums):
+                temp = nums[i]
+                nums[i] = nums[y]
+                nums[y] = nums[x]
+                nums[x]=temp
+                x+=1
+                y+=1
+            elif nums[i]==1 and y<len(nums) and x<len(nums):
+                temp = nums[i]
+                nums[i] = nums[y]
+                nums[y] = temp
+                y+=1
 
-        while -1 in nums: 
-            nums.remove(-1)
             
-        
-        
